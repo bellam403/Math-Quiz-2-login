7855dc71629052262f6d9e59fb41c2415ed5f694
bellam403 <75475386+bellam403@users.noreply.github.com>
2021-02-21 10:49:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function addUser() {
-  player1_name = document.getElementById("player1_name_input").value;
-  player2_name = document.getElementById("player2_name_input").value;
+  player1_name = document.getElementById("player1").value;
+  player2_name = document.getElementById("player2").value;
 
-    localStorage.setItem("player1_name", player1_name);
-    localStorage.setItem("player2_name", player2_name);
+    localStorage.setItem("player1", player1_name);
+    localStorage.setItem("player2", player2_name);
 
     window.location = "https://bellam403.github.io/Math-Quiz-2/";
 }
